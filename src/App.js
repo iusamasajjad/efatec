877cb057f8ea909ba541f9f83bffb2185faa2bf4
Usama Sajjad <i.usamasajjad@gmail.com>
2022-10-29 04:06:00 +0500

    warnings resolved

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from './container/landing-page';
 import Worker from './container/worker/Worker';
-import AOS from 'aos';
+
 import 'aos/dist/aos.css';
 import './App.css';
-import { gsap } from "gsap";
+
 
 function App() {
   return (
--- src/components/SideHamburger/SideHamburger.jsx
+++ src/components/SideHamburger/SideHamburger.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
 import bar from "../../assets/images/bar.png";
-import { gsap } from "gsap";
+
 const SideHamburger =()=>{
 
     return (
